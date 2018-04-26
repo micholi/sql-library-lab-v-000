01e15ec46a96ff928aa47cc39f15f0b15e7d9d77
Michelle Olivieri <michelleo72@gmail.com>
2018-04-26 12:44:18 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,24 +1,26 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY
-  
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
 );
 
 CREATE TABLE subgenre (
   id INTEGER PRIMARY KEY
-  
+
 );
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY
-  
+
 );
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY
-  
+
 );
 
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY
-  
+
 );
