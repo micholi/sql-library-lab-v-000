0619037b183f65458f1a145bac222d99790bceca
Michelle Olivieri <michelleo72@gmail.com>
2018-04-26 13:04:08 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE INTO characters VALUES SELECT characters.species FROM characters ORDER BY characters.id DESC LIMIT 1;
+UPDATE characters.species SELECT characters.species FROM characters ORDER BY characters.id DESC LIMIT 1;
